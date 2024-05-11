aa7f9c375b1557bbee9f063796331e7de9bd4d49
suthardarshankumar <suthardarshan2782@gmail.com>
2024-05-11 17:28:18 +0530

    Done fs.appendFile

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 
-fs.writeFile("hey.txt", "Hey Bro", function (err) {
+fs.appendFile("hey.txt", " I'm Good", function (err) {
     if (err) console.error(err);
     else console.log(`Done`);
 })
