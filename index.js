fd046a6457caa21333614b2d1fdfa838cb75ccf3
suthardarshankumar <suthardarshan2782@gmail.com>
2024-05-12 21:03:07 +0530

    Express Router

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
-// install first express (npm i express)
-
 const express = require('express');
 const app = express();
 
-app.get('/', function (req, res) {
-    res.send('Hello World')
+
+// app get(routes, requestHandler and middleware(is a function))
+
+// Create a Router.
+app.get("/", function (req, res) {
+    res.send(`Hello form Home Page.`);
 })
 
-app.listen(3030)
+// nodemon index.js -- for run a nodemon.
+
+app.get("/profile", function (req, res) {
+    res.send('Hello form Profile Page.')
+})
+
+// Run on this port.
+app.listen(4000);
+
+//Note
+// middleware is perform a some tast or code before to routing.
