3d7fc5c3aadf1381ca2cf608673750e40c863965
Suthar Darshan <suthardarshan2782@gmail.com>
2024-06-11 14:38:42 +0530

    Router, Nodemon, Middlware.

--- index.js
+++ index.js
@@ -2,21 +2,5 @@ const express = require('express');
 const app = express();
 
 
-// app get(routes, requestHandler and middleware(is a function))
 
-// Create a Router.
-app.get("/", function (req, res) {
-    res.send(`Hello form Home Page.`);
-})
-
-// nodemon index.js -- for run a nodemon.
-
-app.get("/profile", function (req, res) {
-    res.send('Hello form Profile Page.')
-})
-
-// Run on this port.
-app.listen(4000);
-
-//Note
-// middleware is perform a some tast or code before to routing.
+app.listen(4040);
